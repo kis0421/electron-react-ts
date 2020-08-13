c4e6a81261108e677405c3c279cc832ce75dac6e
김민규 <kimminkyu@ownerclan.com>
2020-08-13 10:52:43 +0900

    test

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,7 +1,7 @@
-import { app, BrowserWindow } from "electron";
+import { app, BrowserWindow, dialog, Menu, Tray } from "electron";
 import * as path from "path";
 import { format as formatUrl } from "url";
-
+const tray = null;
 const isDevelopment = process.env.NODE_ENV !== "production";
 
 // global reference to mainWindow (necessary to prevent window from being garbage collected)
@@ -29,7 +29,7 @@ function createMainWindow() {
   }
 
   window.on("closed", () => {
-    mainWindow = null;
+    // mainWindow = null;
   });
 
   window.webContents.on("devtools-opened", () => {
@@ -43,10 +43,21 @@ function createMainWindow() {
 }
 
 // quit application when all windows are closed
-app.on("window-all-closed", () => {
+app.on("window-all-closed", (e: Event) => {
   // on macOS it is common for applications to stay open until the user explicitly quits
   if (process.platform !== "darwin") {
-    app.quit();
+    const choice = dialog.showMessageBox({
+      type: "question",
+      buttons: ["Yes", "No"],
+      title: "Confirm",
+      message: "다팔자를 종료하시겠습니까?",
+    });
+    if (choice === 1) {
+      e.preventDefault();
+    } else {
+      app.quit();
+    }
+
   }
 });
 
@@ -60,4 +71,18 @@ app.on("activate", () => {
 // create main BrowserWindow when electron is ready
 app.on("ready", () => {
   mainWindow = createMainWindow();
+  // tray
+  // tray = new Tray(`${__dirname}/static/shibe.png`);
+  // const contextMenu = Menu.buildFromTemplate([
+  //   { label: "Item1", type: "separator" },
+  //   {
+  //     label: "Item2", click() {
+  //       console.log("dd");
+  //     },
+  //   },
+  //   { label: "Item3", type: "radio", checked: true },
+  //   { label: "Item4", type: "radio" },
+  // ]);
+  // tray.setToolTip("이것은 나의 애플리케이션 입니다!");
+  // tray.setContextMenu(contextMenu);
 });
--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -2,6 +2,6 @@ import * as React from "react";
 
 export default class extends React.Component {
   public render() {
-    return <div>Hello World!</div>;
+    return <div>Hello Worldㄴ!</div>;
   }
 }
